dfd886b2bc08e8b1257ecd968b4d65b3b36a8385
UserOfNames <lightninguy.yoyo@gmail.com>
2025-03-02 20:35:24 -0500

    interpret: read lines

--- src/interpret.c
+++ src/interpret.c
@@ -5,11 +5,19 @@
 
 #define EXIT_INVALID_PATH 5
 
+#define MAX_LINE_LENGTH 128
+
 int interpret(char* path_str) {
+    char line[MAX_LINE_LENGTH];
+
     FILE *path = fopen(path_str, "r");
     if (path == NULL) {
         fprintf(stderr, "Error: Could not open path %s\n", path_str);
         exit(EXIT_INVALID_PATH);
+    }
+
+    while (fgets(line, MAX_LINE_LENGTH, path) != NULL) {
+        ;
     }
 
     fclose(path);
