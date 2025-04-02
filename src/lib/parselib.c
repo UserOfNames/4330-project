33e9ea8c20f18d7b4a96b400465c5a49ef764889
UserOfNames <lightninguy.yoyo@gmail.com>
2025-04-02 06:58:47 -0400

    SQUASH parselib start

--- src/lib/parselib.c
+++ src/lib/parselib.c
@@ -13,7 +13,43 @@ Token *_IP;
 long _length;
 
 
-// Evaluate an arithmetic expression; _ip should be pointing to a NUMBER
+// When IP finds a number, that indicates an arithmetic expression.
+// When this is called, IP should be pointing to that first number.
+// This evaluates the expression using a modified shunting-yard algorithm
 double expression() {
-    
+    Queue output = make_queue();
+    // Note that this stack implementation was made to track pointers to
+    // tokens, not tokens. It would be better to track the tokens themselves,
+    // but C sucks for generic programming and making an entire other stack for
+    // this would be bulky and bug-prone, so I'm just doing this.
+    Stack operators = make_stack();
+
+    switch (_IP -> type) {
+        case LPAREN:
+            break;
+
+        case RPAREN:
+            break;
+
+        case PLUS:
+            break;
+
+        case MINUS:
+            break;
+
+        case STAR:
+            break;
+
+        case SLASH:
+            break;
+
+        case NUMBER:
+            enqueue(&output, _IP -> literal.Number);
+            break;
+
+        // All other types are invalid for an arithmetic expression,
+        // so finish evaluating
+        default:
+            break;
+    }
 }
